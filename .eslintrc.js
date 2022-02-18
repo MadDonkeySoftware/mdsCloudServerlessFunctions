3ef5978393a0ff4cc5355f20ef184bbb9713b26a
Frito <fritogotlayed@gmail.com>
2022-02-18 11:06:47 -0600

    Initial prettier install

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,25 @@ module.exports = {
     node: true,
     mocha: true,
   },
-  extends: 'airbnb-base',
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 2018,
   },
-  rules: {},
+  rules: {
+    'no-underscore-dangle': 'off',
+    'max-len': [
+      'error',
+      100,
+      2,
+      {
+        ignoreUrls: true,
+        ignoreComments: true,
+        ignoreRegExpLiterals: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
+  },
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
